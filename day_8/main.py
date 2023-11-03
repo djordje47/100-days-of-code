98f9370fc8dc8f8beb42662939d2487cd163a780
Djordje Arsenovic <dj.arsenovic@gmail.com>
2023-11-03 16:20:23 +0100

    Added day 8

---
+++ day_8/main.py
@@ -0,0 +1,16 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet(name):
+#     print(f'Hi {name}, how are you?')
+#     print(f'Hello {name}')
+#     print(f'{name} can you hear me?')
+# greet('Djole')
+
+def greet_with(name, location):
+    print(f'Hello, {name} are you at {location}')
+
+
+greet_with(location='work', name='Jack')
